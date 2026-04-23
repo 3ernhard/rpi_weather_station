5a3f0e049fb964a4983341aa5034a725ac46dd43
3ernhard <bernhard-hoerl@gmx.at>
2026-04-23 16:41:34 +0200

    improved widget

--- rasp_controller_widget.py
+++ rasp_controller_widget.py
@@ -12,30 +12,22 @@ def print_result(n, s):
 def main():
     head = "time,t_outside,t_inside,pressure,humidity".split(',')
     unit = "%Y-%m-%dT%H:%M:%S,°C,°C,hPa,%".split(',')
-    data = [[], [], [], [], []]
+    data = []
 
     # use all csv files in ./data/
     csvs = sorted(glob(os.path.dirname(os.path.realpath(__file__))+'/data/????-??-??T??-??-??.csv'))
     for csv in csvs:
         with open(csv, "r") as f:
-            for line in f.readlines():
-                if line.startswith('#'):
-                    continue
-                try:
-                    items = line.strip().split(',')
-                    data[0].append(datetime.datetime.strptime(items[0], unit[0]))
-                    for i in range(1, 5):
-                        data[i].append(float(items[i]))
-                except ValueError:
-                    print("=======================================")
-                    print(f"Invalid data line in '{csv}' detected:")
-                    print(f"{line}")
-                    print("=======================================")
+            line = f.readlines()[-1]
+            items = line.strip().split(',')
+            data.append(datetime.datetime.strptime(items[0], unit[0]))
+            for i in range(1, 5):
+                data.append(float(items[i]))
 
-    print_result(1, data[0][-1].strftime("%H:%M"))
-    print_result(2, data[1][-1])
-    print_result(3, data[2][-1])
-    print_result(4, data[4][-1])
+    print_result(1, data[0].strftime("%H:%M"))
+    print_result(2, data[1])
+    print_result(3, data[2])
+    print_result(4, data[4])
 
 
 if __name__ == "__main__":
